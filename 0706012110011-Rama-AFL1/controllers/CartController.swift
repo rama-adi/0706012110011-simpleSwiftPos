08b2ba7ed5d0f6f17165b777cea797100ab075ce
rama-adi <17980909+rama-adi@users.noreply.github.com>
2023-03-03 10:09:44 +0700

    wip

--- 0706012110011-Rama-AFL1/controllers/CartController.swift
+++ 0706012110011-Rama-AFL1/controllers/CartController.swift
@@ -8,6 +8,73 @@
 import Foundation
 
 
+func CartController_ShowCart() {
+    print("Your orders:")
+    
+    var total = 0
+    
+    if Consts_ShoppingCart.isEmpty {
+        print("Your shopping cart is still empty")
+        print("\nPress [return] to go back to main screen")
+        let _ = readLine()
+        
+        MainScreenController_ShowMainScreen()
+    
+    }
+    
+    
+    Consts_ShoppingCart.forEach({ (item, amount) in
+        print("- \(item.name) (\(item.price) ea) x\(amount) - \(item.price * amount)")
+        total += (item.price * amount)
+    })
+    
+    if !Consts_ShoppingCart.isEmpty {
+        print("Total price: \(total)")
+        let confirmPaymentNow = ConsoleAskUtil_Confirm(question: "Do you want to pay now?")
+        
+        if confirmPaymentNow {
+            confirmPayment(total: total)
+        } else {
+            MainScreenController_ShowMainScreen()
+        }
+    }
+}
+
+static func CartController_ConfirmPayment(total: Int) {
+    let money = ConsoleAskUtil_Integer(question: "Please enter the amount of money: ")
+    
+    if money.value < 0 {
+        print("Amount is invalid! \n")
+        confirmPayment(total: total)
+    }
+    
+    if money.value == 0 {
+        print("Amount cannot be zero! \n")
+        confirmPayment(total: total)
+    }
+    
+    if money.value < total {
+        print("Insufficient payment amount for this transaction! (need \(total)) \n")
+        confirmPayment(total: total)
+    }
+    
+    print("Your total order: \(total)")
+    
+    
+    if money.value > total {
+        print("You pay: \(money) Change: \(money.value - total)")
+    } else {
+        print("You pay: \(money) (no change)")
+    }
+    
+    Consts_ShoppingCart = [:]
+    print("Enjoy your meals!")
+    print("\n\nPress [return] to go back to main screen")
+    let _ = readLine()
+    
+    MainScreenController_ShowMainScreen()
+}
+
 struct CartController {
     static func showCart() {
         print("Your orders:")
