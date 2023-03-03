5b9ec2b9db7b2bd62ceef14eafab5a51c4cff737
rama-adi <17980909+rama-adi@users.noreply.github.com>
2023-03-03 09:54:48 +0700

    struct the evil of mankind

--- 0706012110011-Rama-AFL1/controllers/MainScreenController.swift
+++ 0706012110011-Rama-AFL1/controllers/MainScreenController.swift
@@ -7,36 +7,37 @@
 
 import Foundation
 
-struct MainScreenController {
-    static func showMainScreen() -> Void {
-        print("Welcome to UC Walk Cafeteria 👨🏼‍🍳👨🏼‍🍳")
-        
-        var options = Model_Shops_GetShops.map({ shop in
-            return ConsoleAskUtil_Option(value: String(shop.ID), label: shop.name)
-        })
-        
-       
-        options += [
-            ConsoleAskUtil_Option(value: "", label: "-", isSeparator: true),
-            ConsoleAskUtil_Option(value: "S", label: "hopping cart"),
-            ConsoleAskUtil_Option(value: "Q", label: "uit")
-        ]
-        
 
-        let select = ConsoleAskUtil_Dropdown(
-            question: "Please choose cafeteria:",
-            options: options
-        )
-        
-        
-        switch select.value {
-        case "S":
-            return CartController.showCart()
-        case "Q":
-            exit(0)
-        default:
-            let shop = Model_Shops_GetShops[ Int(select.value)! - 1 ]
-            OrderController.startOrder(shop: shop)
-        }
+
+
+func MainScreenController_ShowMainScreen() -> Void {
+    print("Welcome to UC Walk Cafeteria 👨🏼‍🍳👨🏼‍🍳")
+    
+    var options = Model_Shops_GetShops.map({ shop in
+        return ConsoleAskUtil_Option(value: String(shop.ID), label: shop.name)
+    })
+    
+   
+    options += [
+        ConsoleAskUtil_Option(value: "", label: "-", isSeparator: true),
+        ConsoleAskUtil_Option(value: "S", label: "hopping cart"),
+        ConsoleAskUtil_Option(value: "Q", label: "uit")
+    ]
+    
+
+    let select = ConsoleAskUtil_Dropdown(
+        question: "Please choose cafeteria:",
+        options: options
+    )
+    
+    
+    switch select.value {
+    case "S":
+        return CartController.showCart()
+    case "Q":
+        exit(0)
+    default:
+        let shop = Model_Shops_GetShops[ Int(select.value)! - 1 ]
+        OrderController.startOrder(shop: shop)
     }
 }
